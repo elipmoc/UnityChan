2054eb178d6174b23012111f6f9e0e42a037736c
パン粉 ライ麦 <relipmoc101@gmail.com>
2018-01-05 00:44:54 +0900

    PlayerActionのためのGUIを作成

---
+++ Assets/Scripts/PlayerActionGUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//playerが何らかのオブジェクトに対してアクションが行える時、そのアクションができることを画面に表示する
+public class PlayerActionGUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
