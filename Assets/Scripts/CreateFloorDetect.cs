fbd879b59c315841ea643709a89827bdb3f6ed81
パン粉 ライ麦 <relipmoc101@gmail.com>
2018-01-07 14:14:38 +0900

    CreateFloorのスクリプト作成

---
+++ Assets/Scripts/CreateFloorDetect.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateFloorDetect :BaseDetectCube {
+    [SerializeField]
+    private List<GameObject> floorList;
+
+    public override void On()
+    {
+        base.On();
+        floorList.ForEach(x=>x.SetActive(true));
+    }
+
+    public override void Off()
+    {
+        base.Off();
+        floorList.ForEach(x => x.SetActive(false));
+    }
+
+}
