effbb0b862bad7cca05f7f8269eb578c9c6fa6ba
パン粉 ライ麦 <relipmoc101@gmail.com>
2018-01-07 18:28:13 +0900

    goalシーンできた

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadSceneAsync("Goal");   
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
